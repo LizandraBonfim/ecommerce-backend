860c2fba520be28ee633ae394e44b64d2c21852d
Lizandra de Oliveira Bonfim <lizandra@buildbox.com.br>
2021-03-10 20:37:54 -0300

    Add interface de erro

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,9 +1,21 @@
+import { InternalError } from '@src/util/internal-error'
 import { AxiosStatic } from 'axios'
 
+export class ClientRequestError extends InternalError {
+	constructor(message: string) {
+		const internalMessage = `Erro : `
+		super(`${internalMessage} : ${message}`)
+	}
+}
 export class StormGlass {
-	constructor(protected request: AxiosStatic){ }
+	constructor(protected request: AxiosStatic) {}
 	public async fetchPoints(): Promise<{}> {
-		return Promise.resolve({})
-        // return this.request.get(``)
+		try {
+			const response = await this.request.get('colocar a api aqui')
+			return Promise.resolve({})
+			// return this.request.get(``)
+		} catch (error) {
+			throw new ClientRequestError(error.message)
+		}
 	}
 }
