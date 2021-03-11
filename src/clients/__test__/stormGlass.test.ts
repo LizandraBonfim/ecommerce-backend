2e3231f26d903c1a83b63d879e64fdbcc7702c77
Lizandra de Oliveira Bonfim <lizandra@buildbox.com.br>
2021-03-10 21:49:03 -0300

    Add axios refactor

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,11 +1,25 @@
 import { StormGlass } from '@src/clients/stormGlass'
-import axios from 'axios'
+import * as HTTPUtil from '@src/util/request'
 
-jest.mock('axios')
+const data = {
+	name: 'Julia',
+	password: 'Portal123!',
+	gender: 'Feminino',
+	cellphone: '11966596795',
+	documentNumber: '222080164',
+	email: 'julia@email.com',
+}
+jest.mock('@src/util/request')
 describe('StormGlass client', () => {
+	const MochedRequestClass = HTTPUtil.Request as jest.Mocked<
+		typeof HTTPUtil.Request
+	>
+	const mockedRequest = new HTTPUtil.Request() as jest.Mocked<HTTPUtil.Request>
 	it('should return the normalized users from the StormGlass service', async () => {
-		axios.get = jest.fn().mockResolvedValue({})
-		const stormGlass = new StormGlass(axios)
+		mockedRequest.get.mockResolvedValue({
+			data: data,
+		} as HTTPUtil.Response)
+		const stormGlass = new StormGlass(mockedRequest)
 		const response = await stormGlass.fetchPoints()
 		expect(response).toEqual({})
 	})
--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,17 +1,30 @@
-import { InternalError } from '@src/util/internal-error'
-import { AxiosStatic } from 'axios'
-
+import { InternalError } from '@src/util/erros/internal-error'
+import config, { IConfig } from 'config'
+import * as HTTPUtil from '@src/util/request'
 export class ClientRequestError extends InternalError {
 	constructor(message: string) {
 		const internalMessage = `Erro : `
 		super(`${internalMessage} : ${message}`)
 	}
 }
+
+export class ClientResponseError extends InternalError {
+	constructor(message: string) {
+		const internalMessage = `Erro : `
+		super(`${internalMessage} : ${message}`)
+	}
+}
+
+const stormGlassResourceConfig: IConfig = config.get('App.resources.StormGlass')
 export class StormGlass {
-	constructor(protected request: AxiosStatic) {}
+	constructor(protected request = new HTTPUtil.Request()) {}
 	public async fetchPoints(): Promise<{}> {
 		try {
-			const response = await this.request.get('colocar a api aqui')
+			const response = await this.request.get('colocar a api aqui', {
+				headers: {
+					Authorization: stormGlassResourceConfig.get('apiToken'),
+				},
+			})
 			return Promise.resolve({})
 			// return this.request.get(``)
 		} catch (error) {
